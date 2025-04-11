da75ef81eacacd2b260ccd81be36cc087a460ff1
MateusF03 <ferrarijuliomateus@gmail.com>
2025-04-11 10:13:51 -0300

    Adiciona enumeração de marcos corporais e validações de exercícios

--- app/src/main/java/br/edu/uscs/fitcorrect/exercise/ExerciseValidation.kt
+++ app/src/main/java/br/edu/uscs/fitcorrect/exercise/ExerciseValidation.kt
@@ -1,3 +1,8 @@
 package br.edu.uscs.fitcorrect.exercise
 
-data class ExerciseValidation()
+data class ExerciseValidation(
+    val anglePoints: Triple<BodyLandmark, BodyLandmark, BodyLandmark>,
+    val minAngle: Float,
+    val maxAngle: Float,
+    val errorMargin: Float = 5f
+)
